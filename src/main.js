42b8bd4e13d9d646bdec79066d478a74b2c4deba
dan kiss and David Lewis <david@>
2018-03-19 15:57:08 -0700

    add water plant function

--- src/main.js
+++ src/main.js
@@ -1,36 +1,23 @@
 import { Plant } from './plant.js';
 import $ from 'jquery';
+import 'bootstrap/dist/css/bootstrap.min.css';
 // import './styles.css';
+// import 'bootstrap';
+
+let newPlant = null
 
 $(document).ready(function() {
   $('#new-plant').submit(function(event) {
     event.preventDefault();
     let name = $('#name').val();
-    let newPlant = new Plant(name);
+    newPlant = new Plant(name);
     newPlant.growTimeout();
-
-
+    $('#plants').append("<li>" + newPlant.name + "</li>");
     let problem = null;
-    // function plantLife() {
-    //   this.health = 0;
-    //   setInterval(() => {
-    //     const error = "PLANT IS ABOUT TO DIEEEEEE";
-    //     this.health -= 5;
-    //     this.height -=1;
-    //     console.log(this.health, 'top');
-    //     if (this.health < 10) {
-    //       console.log(this.health, ' here?');
-    //       return problem = new Error(error);
-    //     }
-    //   }, 2000);
-    // }
-
     newPlant.plantLife();
-    console.log(newPlant);
     try {
-      // debugger;
-      // const issue = newPlant.plantLife();
-      if (newPlant.problem instanceof Error) {
+      const issue = newPlant.problem;
+      if (issue instanceof Error) {
         console.log('boom');
         console.log(newPlant.problem.message, 22);
         throw Error('plant is dying');
@@ -40,8 +27,12 @@ $(document).ready(function() {
     } catch (error) {
       console.error(`Problem here is the error message!!!! ${error.message}`);
     }
-
-
-
   });
+
+$('#water').click(function() {
+  console.log(newPlant)
+    newPlant.water();
+  });
+
+  
 });
